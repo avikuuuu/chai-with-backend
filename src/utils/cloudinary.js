f5e6c1230314cb6e0bf69921ffe87f23fa38fb60
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2024-03-05 10:10:02 +0530

    cloudinary utility done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_cloud_name,
+  api_key: process.env.CLOUDINARY_api_key,
+  api_secret: process.env.CLOUDINARY_api_secret,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file has been uploaded successfully", response.url);
+    return response;
+  } catch (err) {
+    fs.unlinkSync(localFilePath)
+  }
+};
