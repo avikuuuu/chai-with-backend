ca9c00ed4c5f872c9dc2dfef3e9072d02ecafcb9
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2024-03-05 15:26:16 +0530

    multer setup done

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,13 @@
+// Define an asynchronous request handler using a higher-order function
 const AsyncHandler = (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  // The middleware function that handles asynchronous request handlers
+  return (req, res, next) => {
+    // Create a new Promise that resolves the requestHandler function
+    Promise.resolve(requestHandler(req, res, next))
+      // If there's an error, pass it to the next error handling middleware
+      .catch((err) => next(err));
   };
-};
-
-
-export {AsyncHandler}
+ };
+ 
+ // Export the AsyncHandler function
+ export { AsyncHandler };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,22 +1,28 @@
 import { v2 as cloudinary } from "cloudinary";
-import { response } from "express";
 import fs from "fs";
 
+// Configures Cloudinary with the provided environment variables
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_cloud_name,
-  api_key: process.env.CLOUDINARY_api_key,
-  api_secret: process.env.CLOUDINARY_api_secret,
+ cloud_name: process.env.CLOUDINARY_cloud_name,
+ api_key: process.env.CLOUDINARY_api_key,
+ api_secret: process.env.CLOUDINARY_api_secret,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
-  try {
-    if (!localFilePath) return null;
-    const response = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: "auto",
-    });
-    console.log("file has been uploaded successfully", response.url);
-    return response;
-  } catch (err) {
-    fs.unlinkSync(localFilePath)
-  }
+ try {
+   if (!localFilePath) return null; 
+
+   // Uploads the local file to Cloudinary with auto resource type detection
+   const response = await cloudinary.uploader.upload(localFilePath, {
+     resource_type: "auto",
+   });
+
+   console.log("file has been uploaded successfully", response.url);
+
+
+   return response;
+ } catch (err) {
+   // Deletes the local file if an error occurs during the upload process
+   fs.unlinkSync(localFilePath);
+ }
 };
